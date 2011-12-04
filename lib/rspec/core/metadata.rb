383dc929ef5dfc3f22fbd39943257acbcd3e4cf0
Sasha <sashainkiev@gmail.com>
2011-12-04 15:51:23 +0200

    remove protected #configure_for_example and refactor #for_example

--- lib/rspec/core/metadata.rb
+++ lib/rspec/core/metadata.rb
@@ -128,7 +128,10 @@ module RSpec
 
       # @api private
       def for_example(description, user_metadata)
-        dup.extend(ExampleMetadataHash).configure_for_example(description, user_metadata)
+        example_metadata = dup.extend(ExampleMetadataHash)
+        example_metadata.store(:description_args, [description])
+        example_metadata.store(:caller, user_metadata.delete(:caller) || caller)
+        example_metadata.update(user_metadata)
       end
 
       # @api private
@@ -182,14 +185,6 @@ module RSpec
         else
           metadata[key].to_s == value.to_s
         end
-      end
-
-    protected
-
-      def configure_for_example(description, user_metadata)
-        store(:description_args, [description])
-        store(:caller, user_metadata.delete(:caller) || caller)
-        update(user_metadata)
       end
 
     private
